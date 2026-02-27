979f5490cadd65f075b7c49f05d81e52beebe0dc
“javier” <javiermontes97@gmail.com>
2026-02-27 18:40:42 +0100

    edit ALLOWED_HOSTS param

--- gestortareas/settings.py
+++ gestortareas/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gestor-tareas-avanzado.onrender.com"]
 
 
 # Application definition
